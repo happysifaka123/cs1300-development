a7427b08c84792e490f2eba3cd7f8d3bdef4057a
happysifaka123 <115197297+happysifaka123@users.noreply.github.com>
2022-12-02 17:01:25 -0500

    Delete src directory

--- src/CartItem.jsx
+++
@@ -1,9 +0,0 @@
-import './CartItem.css';
-
-export default function CartItem({item, updateFunction}){
-    return(
-        <div class = "CartItem"> 
-            {item.name} ${item.price}
-            <button onClick = {() => updateFunction(item)}>Remove</button>
-        </div>)
-}
